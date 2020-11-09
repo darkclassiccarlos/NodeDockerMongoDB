04f342db5689c10c048aa86a834418322f38c1fd
Carlos <carlosand_01@hotmail.com>
2020-11-08 21:34:17 -0500

    update views, routes(idex.routes, notes, users), and udpate indes.js main with inicializations, settings a middlewares and static files

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
-const router = Router()
+const router = Router();
 
 router.get('/', (req, res) => {
     res.send('hello world')
+});
+
+router.get('/about', (req, res) => {
+    res.send('About');
 });
 
 module.exports = router;
